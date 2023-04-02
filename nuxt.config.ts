f68db4ecd4e5f379415f83484403f6f4ea5913a8
Raúl Caro Pastorino <raul@fryntiz.dev>
2023-04-02 02:27:15 +0200

    Enhanced meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,27 @@ export default defineNuxtConfig({
     app: {
         head: {
             meta: [
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { charset: 'utf-8' },
+                { name: 'application-name', content: 'www.raupulus.dev' },
+                { name: 'title', content: 'Raúl Caro Pastorino Web Developer (@raupulus)' },
+                { name: 'description', content: 'Portfolio de presentación con la información de Raúl Caro Pastorino (@raupulus) Desarrollador Web (Chipiona, Andalucía, Cádiz, España)' },
+                { name: 'keywords', content: 'Raúl, Raúl Caro, Raul, Raul Caro, Raúl Caro Pastorino, Raul Caro Pastorino, raupulus, desarrollador, desarrollador web, web, developer, web developer, iot, maker, php, laravel, vue, vue3, vuejs, next, nextjs, next3, nextjs3, js, javascript' },
+                { name: 'author', content: 'Raúl Caro Pastorino' },
+                { name: 'twitter:card', content: 'summary' },
+                { name: 'twitter:site', content: '@Nuxt' },
+                { name: 'twitter:creator', content: '@Nuxt' },
+                { name: 'twitter:title', content: 'Nuxt' },
+                { name: 'twitter:description', content: 'Nuxt' },
+                { name: 'twitter:image', content: 'Nuxt' },
+                { name: 'og:title', content: 'Nuxt' },
+                { name: 'og:type', content: 'website' },
+                { name: 'og:url', content: 'Nuxt' },
+                { name: 'og:image', content: 'Nuxt' },
+                { name: 'og:description', content: 'Nuxt' },
+                { name: 'og:site_name', content: 'Nuxt' },
+                { name: 'og:locale', content: 'es_ES' },
+                { name: 'og:locale:alternate', content: 'en_EN' },
             ],
             htmlAttrs: { dir: 'ltr', lang: 'es' },
             link: [
